50d1ec70029edaf60d65d09a1e16572cd9ca1ffc
Andrius Degutis <andriusdgt@gmail.com>
2020-04-29 15:39:06 +0300

    removed unnecessary annotations

--- src/main/java/com/andriusdgt/thedots/mongoplugin/repository/PointCoordinatesRepository.java
+++ src/main/java/com/andriusdgt/thedots/mongoplugin/repository/PointCoordinatesRepository.java
@@ -3,11 +3,9 @@ package com.andriusdgt.thedots.mongoplugin.repository;
 import com.andriusdgt.thedots.api.model.PointCoordinates;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 public interface PointCoordinatesRepository extends MongoRepository<PointCoordinates, String> {
 
     List<PointCoordinates> findAll();

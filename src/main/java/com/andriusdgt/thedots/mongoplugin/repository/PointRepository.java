515016ac5cf4ca8ae98c460249ef1f3a61cc03c2
Andrius Degutis <andriusdgt@gmail.com>
2020-04-29 20:31:05 +0300

    PointCoordinates renamed to Point, classes made final by default

---
+++ src/main/java/com/andriusdgt/thedots/mongoplugin/repository/PointRepository.java
@@ -0,0 +1,21 @@
+package com.andriusdgt.thedots.mongoplugin.repository;
+
+import com.andriusdgt.thedots.api.model.Point;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface PointRepository extends MongoRepository<Point, String> {
+
+    List<Point> findAll();
+
+    List<Point> findByListId(String listId);
+
+    List<Point> findByListId(String listId, Pageable pageable);
+
+    long countByListId(String listId);
+
+    void deleteByListId(String listId);
+
+}
